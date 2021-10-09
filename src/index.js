767c90281f4edb08c9871a3121e9ab46bd35ebf9
ArelIatchi07 <73144884+ArelIatchi07@users.noreply.github.com>
2021-10-09 11:11:45 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const Swal = require('sweetalert2');
+
 
 
 require('dotenv').config();
@@ -10,6 +13,9 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 
@@ -26,9 +32,10 @@ app.set('view engine', '.hbs');
 app.use(require('./routes/index.routes'));
 
 
-
 // Public
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use(express.static(path.join(__dirname, '../dist/assets/js/sweetalert')));
 
 
 // Starting
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,83 +1,262 @@
 const express = require('express')
 const router = express.Router();
+const sql = require('sqlstoreprocedure');
+
+const pool = new sql(process.env.DB_USER, process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PASSWORD);
 
 
 router.get('/', (req, res) => {
-    let options = {
+    res.render('login');
+});
+
+router.get('/dashboard', (req, res) => {
+    let options = [{
         one: 'active'
-    };
-    res.render('dashboard', options);
+    }];
+    res.render('dashboard', { options });
 });
 
 
-router.get('/personal', (req, res) => {
-    let options = {
+router.get('/personal', async(req, res) => {
+    let options = [{
         two: 'active'
-    };
-    res.render('personal', options)
+    }];
+    let employees = (await pool.exec('SP_GET_ACTIVE_EMPLOYEES')).recordset;
+    let area = (await pool.exec('SP_GET_AREAS')).recordset;
+    let bonos = (await pool.exec('SP_GET_INCOMES')).recordset;
+    let deductions = (await pool.exec('SP_GET_EMPLOYEE_DEDUCTIONS ')).recordset;
+    let entities = (await pool.exec('SP_GET_ENTITIES ')).recordset;
+    let towns = (await pool.exec('SP_GET_TOWNS')).recordset;
+    let payrrolls = (await pool.exec('SP_GET_TYPES_PAYROLLS')).recordset;
+
+    res.render('personal', { employees, options, area, bonos, deductions, entities, towns, payrrolls });
 });
 
-router.get('/planilla', (req, res) => {
-    let options = {
-        three: 'active'
-    };
-    res.render('planilla', options)
-});
-
-router.get('/alorsps', (req, res) => {
-    let options = {
-        four: 'active'
-    };
-    res.render('alorsps', options)
-});
-
-router.get('/starteksps', (req, res) => {
-    let options = {
-        five: 'active'
-    };
-    res.render('starteksps', options)
-});
-
-router.get('/zerovariansps', (req, res) => {
-    let options = {
-        six: 'active'
-    };
-    res.render('zerovariansps', options)
-});
-
-router.get('/starteksps', (req, res) => {
-    let options = {
-        seven: 'active'
-    };
-    res.render('starteksps', options)
-});
-
-router.get('/startektgu', (req, res) => {
-    let options = {
-        eight: 'active'
-    };
-    res.render('startektgu', options)
-});
-
-router.get('/startektguxviaje', (req, res) => {
-    let options = {
-        nine: 'active'
-    };
-    res.render('startektguxviaje', options)
-});
-
-router.get('/bonos', (req, res) => {
-    let options = {
-        ten: 'active'
-    };
-    res.render('bonos', options)
-});
-
-router.get('/deducciones', (req, res) => {
-    let options = {
+router.get('/contratistasact', async(req, res) => {
+    let options = [{
         eleven: 'active'
-    };
-    res.render('deducciones', options)
+    }];
+    let employees = (await pool.exec('SP_GET_CONTRACTORS ')).recordset;
+    let deductions = (await pool.exec('SP_GET_CONTRATIST_DEDUCTIONS ')).recordset;
+
+    res.render('contratistasact', { employees, options, deductions })
+});
+
+
+
+router.get('/contratistasinac', async(req, res) => {
+    let options = [{
+        twelve: 'active'
+    }];
+    let employees = (await pool.exec('SP_GET_INACTIVE_CONTRACTORS')).recordset;
+    res.render('contratistasinac', { employees, options })
+});
+
+
+router.get('/inactivos', async(req, res) => {
+    let options = [{
+        ten: 'active'
+    }];
+    let employees = (await pool.exec('SP_GET_INACTIVE_EMPLOYEES')).recordset;
+    res.render('inactivos', { employees, options })
+});
+
+router.get('/planilla', async(req, res) => {
+    let options = [{
+        three: 'active'
+    }];
+    let entities = (await pool.exec('SP_GET_ENTITIES ')).recordset;
+    let payroll = (await pool.exec('SP_GET_PAYROLLS ')).recordset;
+
+    res.render('planilla', { options, entities, payroll })
+});
+router.get('/SanPedroSula', async(req, res) => {
+    let options = [{
+        four: 'active'
+    }];
+
+    let companies = (await pool.exec('SP_GET_COMPANIES_SPS')).recordset;
+    let prices = (await pool.exec('SP_GET_COMPANY_TOWN_PRICES', { companyId: req.params.companyId })).recordset;
+
+    res.render('SanPedroSula', { options, companies, prices })
+});
+
+
+
+
+router.get('/getPrices/:companyId', async(req, res) => {
+
+    let prices = (await pool.exec('SP_GET_COMPANY_TOWN_PRICES', { companyId: req.params.companyId })).recordset;
+
+    res.send(prices);
+});
+
+router.get('/getPricesTgu/:companyId', async(req, res) => {
+
+    let prices = (await pool.exec('SP_GET_COMPANY_DESTINATION_PRICES', { companyId: req.params.companyId })).recordset;
+
+    res.send(prices);
+});
+
+router.get('/editUseTransporIncova', async(req, res) => {
+
+    let prices = (await pool.exec('SP_GET_INCOVA_TOWN_PRICES')).recordset;
+
+    res.send(prices);
+});
+
+
+//edit price sps
+router.post('/sendEditPricesSPS', async(req, res) => {
+
+    try {
+        (await pool.exec('SP_EDIT_COMPANY_TOWN_PRICE', req.body));
+
+        res.status(200).send({
+            ok: true,
+            type: 'success',
+            title: 'Guardado!',
+            message: 'Con éxito',
+        });
+    } catch (err) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+
+});
+
+//edit price tgu
+router.post('/sendEditPricesTGU', async(req, res) => {
+
+    try {
+        (await pool.exec('SP_EDIT_COMPANY_DESTINATION_PRICE', req.body));
+        console.log(req.body);
+        res.status(200).send({
+            ok: true,
+            type: 'success',
+            title: 'Guardado!',
+            message: 'Con éxito',
+        });
+    } catch (err) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+
+});
+
+//register area
+router.post('/sendDescriptionArea', async(req, res) => {
+
+    try {
+        (await pool.exec('SP_REGISTER_AREA', req.body));
+        console.log(req.body);
+        res.status(200).send({
+            ok: true,
+            type: 'success',
+            title: 'Guardado!',
+            message: 'Con éxito',
+        });
+    } catch (err) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+
+});
+
+router.get('/Tegucigalpa', async(req, res) => {
+    let options = [{
+        five: 'active'
+    }];
+    let companies = (await pool.exec('SP_GET_COMPANIES_TGU')).recordset;
+    res.render('Tegucigalpa', { options, companies })
+});
+
+
+router.get('/editUseTransporIncovaTgu', async(req, res) => {
+
+    let prices = (await pool.exec('SP_GET_INCOVA_DESTINATION_PRICES')).recordset;
+
+    res.send(prices);
+});
+
+//register area
+router.post('/registerPayroll', async(req, res) => {
+
+    try {
+        let payroll = (await pool.exec('SP_REGISTER_PAYROLL', req.body)).recordset;
+        res.send(payroll);
+    } catch (err) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+
+});
+
+
+
+//show report general
+router.post('/showFirtsReport', async(req, res) => {
+
+    try {
+        let report = (await pool.exec('SP_EMPLOYEES_GENERAL_PAYROLL', req.body)).recordset;
+        let total = (await pool.exec('SP_GET_TOTAL_INCOMES_PAYROLL', req.body)).recordset;
+        let deductions = (await pool.exec('SP_EMPLOYEES_DEDUCTIONS_PAYROLL', req.body)).recordset;
+        let totaldeductions = (await pool.exec('SP_GET_TOTAL_DEDUCTIONS_PAYROLL', req.body)).recordset;
+        let totalpayments = (await pool.exec('SP_GET_TOTAL_PAYMENTS_PAYROLL', req.body)).recordset;
+
+        let algo = [
+            report,
+            total,
+            deductions,
+            totaldeductions,
+            totalpayments
+        ]
+
+        res.send(algo);
+    } catch (err) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+
+});
+
+//show table extra hours general
+
+router.post('/tableExtraHour', async(req, res) => {
+    try {
+        let salaryExtra = (await pool.exec('SP_GET_SALARY_EXTRA_HOURS_PAYROLL', req.body)).recordset;
+        res.send(salaryExtra);
+    } catch (error) {
+        console.log(err);
+        res.send({
+            ok: false,
+            message: "Ha ocurrido un error"
+        });
+    }
+});
+
+
+router.get('/generatePayroll', async(req, res) => {
+    let options = [{
+        three: 'active'
+    }];
+
+    res.render('generatePayroll', { options })
 });
 
 
